de098348b087f7c219639005f2d01f6da5ce4249
Jonathan Emord <jonathan.emord@gmail.com>
2013-06-25 14:58:47 -0500

    Added install script, README, gitignore, and vim plugins.

---
+++ README.md
@@ -0,0 +1,5 @@
+My setups for Xresources (solarized), vim, zsh and git.
+
+To install just use install.sh.
+
+Assumes oh-my-zsh is installed. https://github.com/robbyrussell/oh-my-zsh
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+repodir=`pwd`
+mkdir olddotfiles
+backupdir="./olddotfiles"
+files="Xresources gitconfig vimrc zshrc vim zsh"
+
+for file in $files; do
+    echo "Backing up $file to $backupdir"
+    mv ~/.$file $backupdir
+    echo "Creating symlink to $file in home directory"
+    ln -s $repodir/$file ~/.$file
+done
