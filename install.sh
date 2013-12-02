1c8a2313eb6d8bbd489424319408fa3ab5a30a05
Jonathan Emord <jonathan.emord@gmail.com>
2013-12-02 01:11:23 -0500

    Added tmux conf

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 repodir=`pwd`
 mkdir olddotfiles
 backupdir="./olddotfiles"
-files="Xresources gitconfig vimrc zshrc vim zsh"
+files="Xresources gitconfig vimrc zshrc vim zsh tmux.conf"
 
 for file in $files; do
     echo "Backing up $file to $backupdir"
